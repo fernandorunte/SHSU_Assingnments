9364e8791f9cb2ed39ef3a4a7895e2e2c5ddf3f6
Fernando Runte <fernandoesponton@gmail.com>
2019-03-21 14:47:40 -0500

    Lab over geometrical object calculations

---
+++ C/lab_3/lab_3/mymathlib.h
@@ -0,0 +1,24 @@
+
+#ifndef __MYMATHLIB_H
+#define __MYMATHLIB_H
+
+
+
+
+
+extern void sphere(float radius, float *surface, float *volume);
+extern float volCylinder(float radius, float height);
+extern float sumFloats(float x[], int numFloats);
+extern double sine(float angle);
+
+
+
+
+
+
+
+
+
+#endif
+
+
